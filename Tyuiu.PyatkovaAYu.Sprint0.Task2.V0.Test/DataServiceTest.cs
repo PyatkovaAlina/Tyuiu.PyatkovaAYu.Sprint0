8356cd2a81cd109f3c1d0d9059a5bd1cca3d39de
Tyuiu.PyatkovaAYu@outlook.com <Tyuiu.PyatkovaAYu@outlook.com>
2023-09-17 07:46:58 +0500

    Task2

---
+++ Tyuiu.PyatkovaAYu.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PyatkovaAYu.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.PyatkovaAYu.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Алина";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Алина", res);
+        }
+    }
+}
